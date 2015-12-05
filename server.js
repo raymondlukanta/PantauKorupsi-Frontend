5c89114c13d5d0fe44a1459994ff38443989417a
Raymond Lukanta <raymond_pluto@hotmail.com>
2015-12-05 15:02:58 +0800

    change boilderplate

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+var http = require('http');
+var express = require('express');
+var app = express();
 
-var app = new (require('express'))()
-var port = 4000
+app.use(require('morgan')('short'));
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
+(function initWebpack() {
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack/dev.config');
+  var compiler = webpack(webpackConfig);
 
-app.use(function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true, publicPath: webpackConfig.output.publicPath,
+  }));
 
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
-})
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
+  }));
+
+  app.use(express.static(__dirname + '/'));
+})();
+
+app.get('/', function root(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+if (require.main === module) {
+  var server = http.createServer(app);
+  server.listen(process.env.PORT || 3000, function onListen() {
+    var address = server.address();
+    console.log('Listening on: %j', address);
+    console.log(' -> that probably means: http://localhost:%d', address.port);
+  });
+}
