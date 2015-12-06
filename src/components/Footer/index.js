6aa966548a96ea37445216e4c5a7b00a4465a428
Edward Samuel Pasaribu <edwardsamuel92@gmail.com>
2015-12-06 22:04:12 +0800

    Subscription footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,34 +8,23 @@ const AUTHORS = ['anorudes', 'keske'];
 
 export class Footer extends Component {
   render() {
-    function renderGitHubFollowButton(user) {
-      return (
-        <a className="github-button" href={`https://github.com/${user}`}
-           data-count-href={`/${user}/followers`}
-           data-count-api={`/users/${user}#followers`}
-           data-count-aria-label="# followers on GitHub"
-           aria-label={`Follow @${user} on GitHub`}>
-          @{user}
-        </a>
-      );
-    }
     return (
       <footer className={`${ styles }`}>
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <a className="github-button"
-               href="https://github.com/anorudes/redux-easy-boilerplate"
-               data-icon="octicon-star"
-               data-count-href="/anorudes/redux-easy-boilerplate/stargazers"
-               data-count-api="/repos/anorudes/redux-easy-boilerplate#stargazers_count"
-               data-count-aria-label="# stargazers on GitHub"
-               aria-label="Star anorudes/redux-easy-boilerplate on GitHub">
-               Star
-            </a>
-              {
-                AUTHORS.map((author) => renderGitHubFollowButton(author))
-              }
+            <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4">
+              Silahkan subscribe untuk mendapatkan pembaharuan tentang korupsi, termasuk pengingat kasus korupsi yang tidak ada perkembangannya lagi.
+              <br />
+              <br />
+              <form action="//amazonaws.us1.list-manage.com/subscribe/post?u=e8c2fffc7d17de9ee383de69d&amp;id=811dd4dbb9" method="post" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate className="form-inline">
+                <input type="email" name="EMAIL" placeholder="Email" className="form-control input-sm"/>
+                <input type="submit" value="Subscribe" name="subscribe" className="btn btn-primary btn-sm"/>
+              </form>
+              <br />
+              <br />
+              <a href="http://ec2-52-77-251-73.ap-southeast-1.compute.amazonaws.com">Pantau Pemilu</a> untuk
+              <br />
+              <a href="http://hackathonmerdeka.id">Hackaton Merdeka 3.0</a> dan <a href="http://www.indonesia.go.id">Indonesia</a>.
             </div>
           </div>
         </div>
--- src/containers/OrganizationDetailPage/index.js
+++ src/containers/OrganizationDetailPage/index.js
@@ -5,6 +5,7 @@ import DocumentMeta from 'react-document-meta';
 /* components */
 import { Issue } from 'components/Issue';
 import { Actor } from 'components/Actor';
+import { LoadingPage } from '../LoadingPage';
 
 /*Redux */
 import { loadReadOrganization } from 'actions/organizations';
@@ -70,54 +71,15 @@ export class OrganizationDetailPage extends Component {
 
         var organization
         var issues
-        // var actors
-        if (organizations != undefined) {
+
+        if (organizations !== undefined) {
             organization = organizations[this.organizationId]
-            issues = organization[""]
+            issues = organization.issues
         }
 
-        var issues = [
-            {   "title": "Setya Novanto Dibidik Sangkaan Korupsi",
-                "description": "Kejaksaan Agung membidik Ketua DPR Setya Novanto dengan sangkaan korupsi. Bidikan itu dilakukan terkait dugaan pencatutan nama Presiden Joko Widodo dan Wakil Presiden Jusuf Kalla",
-                "author": "James Reddy",
-                "financialCost": "IDR50.000.000.000",
-                "createdAt": "4 December 2015",
-                "startedAt": "2015-11-08",
-                "status": "Penyelidikan"
-            },
-            {
-                "title": "Terdakwa Korupsi Pergola Dipaksa DPRD",
-                "description": "Kepala Badan Lingkungan Hidup (BLH) Yogyakarta nonaktif, Irfan Susilo mengaku dipaksa oleh pihak legislatif untuk terus menjalankan proyek pergola tahun anggaran 2013",
-                "author": "James Reddy",
-                "financialCost": "IDR10000000000",
-                "createdAt": "4 December 2015",
-                "startedAt": "2015-08-04",
-                "status": "Selesai"
-            },
-            {   "title": "Korupsi Benanga, Ganti Rugi Diubah Jadi Bansos, Tiga Pejabat Pemkot Diperiksa",
-                "description": "Persidangan perkara korupsi bantuan sosial (bansos) Benanga di Pengadilan Tipikor Samarinda, masih berkutat di pemeriksaan saksi",
-                "author": "James Reddy",
-                "financialCost": "IDR20000000000",
-                "createdAt": "4 December 2015",
-                "startedAt": "2015-05-15",
-                "status": "Penyelidikan"
-            }
-        ]
-
-        var actors = [
-            {   "name": "Widjanarko Puspoyo, MA",
-                "description": "Kepala BULOG periode 2001 s/d 2003 / Direktur Utama Perum BULOG, periode 2003 s/d 2007",
-                "profile_url": ""
-            },
-            {   "name": "Ir. Jamerdin Purba",
-                "description": "Pegawai Negeri Sipil / Kepala Balai Pembibitan Ternak Unggul (BPTU) Babi dan Kerbau Siborongborong (SK Menteri Pertanian tanggal 25 Januari 2005)",
-                "profile_url": ""
-            },
-            {   "name": "Ir. Yulianus Telaumbanua",
-                "description": "Pegawai Negeri Sipil/ Ketua Panitia Pelelangan, Pembelian/Pengadaan Ternak Kerbau Lokal pada Balai Pembibitan Ternak Unggul (BPTU) Babi dan Kerbau Siborongborong tahun 2005",
-                "profile_url": ""
-            }
-        ]
+        if (organization === undefined) {
+            return (<LoadingPage />);
+        }
 
         return (
             <section>
@@ -132,11 +94,9 @@ export class OrganizationDetailPage extends Component {
                             </div>
                         </div>
                         <div className="row issue-detail-tab">
-                            <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8
-                            col-md-offset-2 col-lg-offset-2">
+                            <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
                                 <ul className="nav nav-tabs">
                                     <li className="active"><a data-toggle="tab" href="#organization-issues">Kasus Terkait</a></li>
-                                    <li><a data-toggle="tab" href="#organization-actors">Pihak Terkait</a></li>
                                 </ul>
 
                                 <div className="tab-content">
@@ -152,20 +112,7 @@ export class OrganizationDetailPage extends Component {
                                             </tbody>
                                         </table>
                                     </div>
-                                    <div id="organization-actors" className="tab-pane fade">
-                                        <table>
-                                            <tbody>
-                                                {!actors.length && <span>Tidak tersedia</span>}
-                                                {
-                                                    actors.map((actor, index) =>
-                                                        <Actor actor={actor}/>
-                                                    )
-                                                }
-                                            </tbody>
-                                        </table>
-                                    </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
