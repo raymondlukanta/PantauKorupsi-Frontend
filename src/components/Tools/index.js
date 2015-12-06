31972439d1e5e3eeb2612f68e62d950e8431b13d
Anasthasia Amelia <anasthasia.amelia@gmail.com>
2015-12-07 00:41:56 +0800

    edit layout for Home

--- src/components/Tools/index.js
+++ src/components/Tools/index.js
@@ -1,76 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 /* component styles */
 import styles from './styles';
 
 /* images */
-const reactjs = require('./files/reactjs.png');
-const redux = require('./files/redux.png');
-const babel = require('./files/babel.png');
-const webpack = require('./files/webpack.png');
-const bootstrap = require('./files/bootstrap.png');
-const mocha = require('./files/mocha.png');
+const issueImg = require('./files/issue.png');
+const actorImg = require('./files/actor.png');
+const organizationImg = require('./files/organization.png');
 
 export class Tools extends Component {
-  render() {
-    return (
-      <section className={`${styles}`}>
-        <div className="container">
+    render() {
+        return (
+            <section className={`${styles}`}>
+                <div className="container">
 
-          <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-              <h2>
-                Boilerplate contains
-              </h2>
-            </div>
-          </div>
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                            <h2>
+                                Telusuri Berbagai Sudut dari Setiap Kasus Korupsi
+                            </h2>
+                        </div>
+                    </div>
 
-          <div className="row">
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={reactjs} />
-              <h4>
-                React
-              </h4>
-            </div>
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 tool">
+                            <img src={issueImg} />
+                            <h3><Link to="/issues">Kasus</Link></h3>
+                            <h4>
+                                Jelajahi dan dapatkan informasi terbaru dari kasus-kasus korupsi yang terjadi di Indonesia.
+                            </h4>
+                        </div>
 
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={redux} />
-              <h4>
-                Redux
-              </h4>
-            </div>
+                        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 tool">
+                            <img src={actorImg} />
+                            <h3><Link to="/actors">Tokoh</Link></h3>
+                            <h4>
+                                Ketahui siapa saja di balik setiap kasus korupsi yang terjadi.
+                            </h4>
+                        </div>
 
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={babel} />
-              <h4>
-                Babel
-              </h4>
-            </div>
+                        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 tool">
+                            <img src={organizationImg} />
+                            <h3><Link to="/organizations">Institusi</Link></h3>
+                            <h4>
+                                Dapatkan informasi mengenai institusi yang terkait kasus korupsi.
+                            </h4>
+                        </div>
+                    </div>
 
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={webpack} />
-              <h4>
-                Webpack
-              </h4>
-            </div>
-
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={bootstrap} />
-              <h4>
-                Bootstrap
-              </h4>
-            </div>
-
-            <div className="col-xs-6 col-sm-2 col-md-2 col-lg-2 tool">
-              <img src={mocha} />
-              <h4>
-                Mocha
-              </h4>
-            </div>
-          </div>
-
-        </div>
-      </section>
-    );
-  }
+                </div>
+            </section>
+        );
+    }
 }
